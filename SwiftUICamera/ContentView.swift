45b412e90e6c1d9e868fdf07c90542ac2f5371f5
JYI <jyi20180223@gmail.com>
2023-06-04 22:53:27 +0900

    완성

--- SwiftUICamera/ContentView.swift
+++ SwiftUICamera/ContentView.swift
@@ -9,12 +9,15 @@
 import SwiftUI
 
 struct ContentView: View {
+     var imageViewModel: ImageViewModel = ImageViewModel()
     
     @State private var showSheet: Bool = false
     @State private var showImagePicker: Bool = false
     @State private var sourceType: UIImagePickerController.SourceType = .camera
     
-    @State private var image: UIImage? 
+    
+    @State private var image: UIImage?
+    @State private var label: String = ""
     
     var body: some View {
         
@@ -42,20 +45,99 @@ struct ContentView: View {
                             .cancel()
                         ])
                 }
+              
                 
+                Button("Classify") {
+                    imageViewModel.classifyImage(newImage : self.image)
+                    checkSafe(string: imageViewModel.classificationLabel)
+                                }
+                                .padding()
+                
+                
+                Text(
+                    label
+                )
+                    .padding()
             }
                 
                 
-            .navigationBarTitle("Camera Demo")
+            .navigationBarTitle("Safe Look Book")
             
         }.sheet(isPresented: $showImagePicker) {
+            
             ImagePicker(image: self.$image, isShown: self.$showImagePicker, sourceType: self.sourceType)
+//            ImagePicker(image: self.$imageViewModel.image, isShown: self.$showImagePicker, sourceType: self.sourceType)
+//                .environmentObject(self.imageViewModel)
         }
+
+
+    }
+    
+    func checkSafe(string: String) {
+        let tools = ["helmet", "mask", "vest", "shoe"]
+        let limit : Double = 0.5
+        var results = string.components(separatedBy: "\n")
+        var isExist = ["X", "X", "X","X"]
+        var newLabel = ""
+        
+        
+        for result in results {
+            
+            var startIndex = result.index(result.endIndex, offsetBy: -6)
+            var endIndex = result.index(result.endIndex, offsetBy: -1)
+            var substring = result[startIndex..<endIndex]
+            var trimmedStr = substring.trimmingCharacters(in: .whitespaces)
+            
+            var resultLimit : Double? = Double(trimmedStr)
+            
+            if (resultLimit != nil){
+                if (resultLimit! < limit) {
+                    break
+                }
+            }
+            
+            print(result)
+            
+            
+            for index in 0..<tools.count {
+                if (result.contains(tools[index])) {
+                    isExist[index] = "O"
+                }
+            }
+        }
+//        for resultIdx in 0..<20 {
+//            for index in 0..<tools.count {
+//                if (results[resultIdx].contains(tools[index])) {
+//                    isExist[index] = "O"
+//                }
+//            }
+//        }
+        
+//        for result in results {
+//            for index in 0..<tools.count {
+//                if (result.contains(tools[index])) {
+//                    isExist[index] = "O"
+//                }
+//            }
+////            for tool in tools {
+////                if (result.contains(tool)) {
+////                    newLabel += result + "\n"
+////                    continue loop
+////                }
+////            }
+//        }
+        for index in 0..<tools.count {
+            newLabel += tools[index] + " : " + isExist[index] + "\n"
+        }
+            
+        
+        label = newLabel
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+                    .environmentObject(ImageViewModel())
     }
 }
